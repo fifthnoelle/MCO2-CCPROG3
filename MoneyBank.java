b62a01d4a4d85ac7a9d6767857046d97481793d8
fifthnoelle <70679663+fifthnoelle@users.noreply.github.com>
2023-08-03 04:29:18 +0800

    final?

--- MoneyBank.java
+++ MoneyBank.java
@@ -1,14 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.mco_gui;
+
 import java.util.*;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class MoneyBank {
     Money money;
-    private ArrayList<Money> changeMoney = new ArrayList<>();
+    public ArrayList<Money> changeMoney = new ArrayList<>();
     private ArrayList<Money> paymentMoney = new ArrayList<>();
     private ArrayList<Money> partialPayment = new ArrayList<>();
-    Scanner scan = new Scanner(System.in);
 
     MoneyBank() {
-       setDenomination();
+       
     }
 
     /**
@@ -31,67 +39,56 @@ public class MoneyBank {
      * the chosen value from all denominations and increments their quantities respectively in partialPayment array
      * @return float
      */
-    public float partialPaymentMenu(){
-        int i, nChoice;
-        float totalCash = 0;
-        
-        System.out.println("\nChoose Denominations:");
-        do{
-        for (i = 0; i < paymentMoney.size(); i++) {
-            Money currMoney = paymentMoney.get(i);
-            System.out.println("[" + (i+1) + "] P" + currMoney.getValue());
+
+
+
+public float partialPaymentMenu() {
+    float totalCash = 0;
+    int nChoice;
+
+    String[] denominationNames = new String[partialPayment.size() + 1];
+    for (int i = 0; i < partialPayment.size(); i++) {
+        denominationNames[i] = "P" + partialPayment.get(i).getValue();
+    }
+    denominationNames[partialPayment.size()] = "Buy";
+
+    do {
+        String choicesMessage = "\nChoose Denominations:\n";
+        /*for (int i = 0; i < partialPayment.size(); i++) {
+            Money currMoney = partialPayment.get(i);
+            choicesMessage += "[" + (i + 1) + "] P" + currMoney.getValue() + "\n";
         }
-        System.out.println("[" + (0) + "] Done");
-        nChoice = scan.nextInt();
-        
-        
-        switch(nChoice){
-            case 1: partialPayment.get(0).setQuantity(partialPayment.get(0).getQuantity() + 1);
-                    totalCash += partialPayment.get(0).getValue();
-                    System.out.println("\nAdded P" + partialPayment.get(0).getValue());
-            break;
-            case 2: partialPayment.get(1).setQuantity(partialPayment.get(1).getQuantity() + 1);
-                    totalCash += partialPayment.get(1).getValue();
-                    System.out.println("\nAdded P" + partialPayment.get(1).getValue()); 
-            break;
-            case 3: partialPayment.get(2).setQuantity(partialPayment.get(2).getQuantity() + 1);
-                    totalCash += partialPayment.get(2).getValue();
-                    System.out.println("\nAdded P" + partialPayment.get(2).getValue());
-            break;
-            case 4: partialPayment.get(3).setQuantity(partialPayment.get(3).getQuantity() + 1);
-                    totalCash += partialPayment.get(3).getValue();
-                    System.out.println("\nAdded P" + partialPayment.get(3).getValue());
-            break;
-            case 5: partialPayment.get(4).setQuantity(partialPayment.get(4).getQuantity() + 1);
-                    totalCash += partialPayment.get(4).getValue();
-                    System.out.println("\nAdded P" + partialPayment.get(4).getValue());
-            break;
-            case 6: partialPayment.get(5).setQuantity(partialPayment.get(5).getQuantity() + 1);
-                    totalCash += partialPayment.get(5).getValue();
-                    System.out.println("\nAdded P" + partialPayment.get(5).getValue());
-            break;
-            case 7: partialPayment.get(6).setQuantity(partialPayment.get(6).getQuantity() + 1);
-                    totalCash += partialPayment.get(6).getValue();
-                    System.out.println("\nAdded P" + partialPayment.get(6).getValue());
-            break;
-            case 8: partialPayment.get(7).setQuantity(partialPayment.get(7).getQuantity() + 1);
-                    totalCash += partialPayment.get(7).getValue();
-                    System.out.println("\nAdded P" + partialPayment.get(7).getValue());
+        choicesMessage += "[0] Done";*/
+
+        // Create a pop-up dialog for selecting the denomination
+        nChoice = JOptionPane.showOptionDialog(
+                null,
+                choicesMessage,
+                "Partial Payment",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                denominationNames,
+                denominationNames[0]);
+
+        if (nChoice >= 0 && nChoice < partialPayment.size()) {
+            int index = nChoice;
+            partialPayment.get(index).setQuantity(partialPayment.get(index).getQuantity() + 1);
+            totalCash += partialPayment.get(index).getValue();
+            JOptionPane.showMessageDialog(null, "\nAdded P" + partialPayment.get(index).getValue(), "Added Payment", JOptionPane.INFORMATION_MESSAGE);
+            System.out.println("Subtotal: " + totalCash + "\n");
+        } else if (nChoice == partialPayment.size()) {
+            // User clicked "Back" button, break the loop
             break;
         }
+    } while (nChoice != JOptionPane.CLOSED_OPTION && nChoice != 0);
 
-        System.out.println("Subtotal: " + totalCash + "\n");
-        }while(nChoice != 0);
-
-        return totalCash;
+    if (nChoice != partialPayment.size()) {
+        JOptionPane.showMessageDialog(null, "Subtotal: " + totalCash + "\n", "Subtotal", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    public void resetPartialPayment(){
-        for (int i = 0; i < partialPayment.size(); i++) {
-            partialPayment.get(i).setQuantity(0);
-            //System.out.println("Quantity of" + partialPayment.get(i).getValue() + " is " + partialPayment.get(i).getQuantity());
-            }
-    }
+    return totalCash;
+}
 
     /**
      * Migrates quantities per denomination from partialPayment to paymentMoney array. It also empties partialPayment
@@ -166,68 +163,88 @@ public class MoneyBank {
      * the user to be updated of the change given to them. If there is not enough change, a
      * warning will be printed urging for maintenance and addPayment will not execute.
      * @param totalChange the total change from the transaction
-     * @return 
      */
-    public boolean makeChange(float totalChange) {
+public boolean makeChange(float totalChange) {
         int i;
         float amt = totalChange;
+        StringBuilder message = new StringBuilder();
+        boolean changeSuccessful = true;
 
-        if(amt>0){
-            System.out.println("\nChange:");
+        if (amt > 0) {
+            message.append("\nChange:\n");
         }
-        for (i = changeMoney.size()-1; i >= 0; i--) {
-            
+        
+        if(amt == 0){
+                message.append("Exact amount paid! \n\nP0");
+            }
+
+        for (i = changeMoney.size() - 1; i >= 0; i--) {
             Money currMoney = changeMoney.get(i);
             float currVal = currMoney.getValue();
             int currQuantity = currMoney.getQuantity();
-            while (amt >= currMoney.getValue() && currMoney.getQuantity() > 0) {
+
+            while (amt >= currVal && currMoney.getQuantity() > 0) {
                 float fQuantity;
                 int quantity;
 
                 fQuantity = amt / currVal;
-                quantity = (int)fQuantity;
+                quantity = (int) fQuantity;
 
                 if (quantity <= currMoney.getQuantity() && quantity != 0) {
+                    amt = amt - (currVal * quantity);
 
-                    amt = amt - (currVal * quantity); 
-                    
-                    if(currMoney.getQuantity() != 0){
-                    System.out.println((int) quantity + " * P" + currVal);
+                    if (currMoney.getQuantity() != 0) {
+                        message.append((int) quantity).append(" * P").append(currVal).append("\n");
                     }
-                    subtractMoney(currVal, (int) quantity); 
-                }
-                else if (quantity > currMoney.getQuantity() && quantity != 0) {
-                    if(currMoney.getQuantity() != 0){
-                    System.out.println(currQuantity + " * P" + currVal);
+                    subtractMoney(currVal, (int) quantity);
+                } else if (quantity > currMoney.getQuantity() && quantity != 0) {
+                    if (currMoney.getQuantity() != 0) {
+                        message.append(currQuantity).append(" * P").append(currVal).append("\n");
                     }
                     subtractMoney(currVal, currQuantity);
                 }
             }
-            
         }
 
-        if(amt > 0){
-            System.out.println("Not enough change! Maintenance Required. (Change Error)");
+        if (amt > 0) {
+            message.append("Not enough change! Maintenance Required. (Change Error)\n");
             resetPartialPayment();
-            return false;
-        }else{
+            changeSuccessful = false;
+        } else {
             addPayment();
-            return true;
         }
+
+        // Show the change result in a pop-up dialog
+        JOptionPane.showMessageDialog(
+                null, message.toString(),
+                "Change Result", JOptionPane.PLAIN_MESSAGE
+        );
+
+        return changeSuccessful;
     }
 
     /**
      * Shows the quantity of denominations to be used for giving out change to the user.
      */
-    public void showChangeStock(){
-        int i;
-        System.out.println("\nCurrent Change Stock");
-        for (i = 0; i < changeMoney.size(); i++) {
-            Money currMoney = changeMoney.get(i);
-            
-            if(currMoney.getQuantity() != 0){
-            System.out.println(currMoney.getQuantity() + "\tP" + currMoney.getValue());
-            }
+    public void showChangeStock() {
+    int i;
+    String stockMessage = "Current Change Stock\n";
+
+    for (i = 0; i < changeMoney.size(); i++) {
+        Money currMoney = changeMoney.get(i);
+
+        if (currMoney.getQuantity() != 0) {
+            stockMessage += currMoney.getQuantity() + "\tP" + currMoney.getValue() + "\n";
+        }
+    }
+
+    // Show the change stock information in a popup dialog
+    JOptionPane.showMessageDialog(null, stockMessage, "Change Stock", JOptionPane.INFORMATION_MESSAGE);
+}
+    public void resetPartialPayment(){
+        for (int i = 0; i < partialPayment.size(); i++) {
+            partialPayment.get(i).setQuantity(0);
+            //System.out.println("Quantity of" + partialPayment.get(i).getValue() + " is " + partialPayment.get(i).getQuantity());
             }
     }
 
@@ -288,5 +305,6 @@ public class MoneyBank {
             }
         return totalChange;
     }
-
+    
+    
 }
